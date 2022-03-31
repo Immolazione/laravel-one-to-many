8353ebb39ca4b33403cd234b5bc0b3397f562c7c
Immolazione <federico.deered@gmail.com>
2022-03-31 19:30:56 +0200

    add links models

--- app/Models/post.php
+++ app/Models/post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class post extends Model
 {
     protected $fillable = ['title', 'content', 'slug', 'created_at', 'updated_at'];
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
 }
